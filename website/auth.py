b1e8974fc35dbbd91a5648f44d5d0382ec7b2870
Mati Nemera <mnemera17@winona.edu>
2022-07-18 19:15:46 -0500

    daatabase working

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,10 @@
 from cgitb import text
 from xmlrpc.client import boolean
-from flask import Blueprint,render_template, request, flash
+from flask import Blueprint,render_template, request, flash,redirect, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash 
+
 
 auth = Blueprint('auth', __name__)
 
@@ -20,13 +24,13 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1= request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 characters.', category='error')
         elif password1 != password2:
             flash('Passwords do not match.', category='error')
@@ -34,5 +38,10 @@ def sign_up():
             flash('Password must be greater than 7 characters.', category='error')
         else:
             #add user to database
+            new_user = User(email = email, password = generate_password_hash(password1, method='sha256'), first_name = first_name)
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account Created!!', category='success')
+            return redirect(url_for('views.home'))
+
     return render_template("signup.html")
